3cbe8278ffd91faefebf7d2fc2d1acae6203469b
Novice_Neoguri <94734089+PNUHCT@users.noreply.github.com>
2022-10-18 19:23:43 +0900

    TodoList_MVC_JPA_server_V3 1. Gets 서비스 메서드 수정

--- src/main/java/SoloProject/SoloProject/Todo/Service/TodoService.java
+++ src/main/java/SoloProject/SoloProject/Todo/Service/TodoService.java
@@ -14,7 +14,7 @@ import java.net.URI;
 import java.util.Optional;
 
 @Service
-//@RequiredArgsConstructor
+//@RequiredArgsConstructor맞나난ㄴ
 public class TodoService {
     private TodoRepository todoRepository;
 
